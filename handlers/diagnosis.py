82cdd6a696856fb9b87168f99c8ed367fadff3ef
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-13 17:18:08 +0700

    updated handler

--- handlers/diagnosis.py
+++ handlers/diagnosis.py
@@ -1,15 +1,10 @@
 
-from connector
+from connector.db import db_dermis
 
 def get_disease_info(condition):
-    with open('secret.json', 'r') as secret_file:
-        db_config = json.load(secret_file)
-    
-    db_dermis = mysql.connector.connect(**db_config)
-
     cursor = db_dermis.cursor()
 
-    query = "SELECT causes, treatment, severity FROM diseases WHERE condition = %s"
+    query = "SELECT description, causes, treatment, severity FROM skin_condition WHERE LOWER(name) = LOWER(%s)"
 
     cursor.execute(query, (condition,))
 
@@ -19,13 +14,16 @@ def get_disease_info(condition):
 
     response = {}
 
-    if result:
-        causes, treatment, severity = result
+    print('got', result)
 
+    if result:
+        description, causes, treatment, severity = result
+
+        response['description'] = description
         response['causes'] = causes
         response['treatment'] = treatment
         response['severity'] = severity
 
         return response
     else:
-        return None, 404.
+        return None, 404
