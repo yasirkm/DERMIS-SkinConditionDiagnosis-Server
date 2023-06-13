7477afbe131a637ccd8db3ce8acc7ab9dcfed56e
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-13 21:46:50 +0700

    bugfix and removed print statement

--- handlers/diagnosis.py
+++ handlers/diagnosis.py
@@ -4,24 +4,23 @@ from connector.db import db_dermis
 def get_disease_info(condition):
     cursor = db_dermis.cursor()
 
-    query = "SELECT description, causes, treatment, severity FROM skin_condition WHERE LOWER(name) = LOWER(%s)"
+    query = "SELECT description, causes, treatments, severity FROM skin_condition WHERE LOWER(name) = LOWER(%s)"
 
     cursor.execute(query, (condition,))
 
     result = cursor.fetchone()
+    cursor.fetchall()
 
     cursor.close()
 
     response = {}
 
-    print('got', result)
-
     if result:
-        description, causes, treatment, severity = result
+        description, causes, treatments, severity = result
 
         response['description'] = description
         response['causes'] = causes
-        response['treatment'] = treatment
+        response['treatments'] = treatments
         response['severity'] = severity
 
         return response
