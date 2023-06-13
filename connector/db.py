dad127cb7a6812b4eb31b0401bf0987f75229a8d
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-13 23:08:14 +0700

    added environment variable configuration

--- connector/db.py
+++ connector/db.py
@@ -1,10 +1,14 @@
-import mysql.connector
+import os
 import json
 from pathlib import Path
+import mysql.connector
 
 secret_path = Path(__file__).parent / 'auth' / 'secret.json'
 
 with open(secret_path, 'r') as secret_file:
     db_config = json.load(secret_file)
+
+db_config['host'] = os.environ.get('DB-HOST')
+db_config['database'] = os.environ.get('DB-NAME')
 
 db_dermis = mysql.connector.connect(**db_config)
