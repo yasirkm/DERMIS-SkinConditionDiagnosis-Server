8e4ea01780d5e2849088a46437311da9f0ee52f7
Yasir Khairul Malik <yasirkm3071@gmail.com>
2023-06-16 19:18:10 +0700

    updated db connector for google cloud run

--- connector/db.py
+++ connector/db.py
@@ -8,7 +8,8 @@ secret_path = Path(__file__).parent / 'auth' / 'secret.json'
 with open(secret_path, 'r') as secret_file:
     db_config = json.load(secret_file)
 
-db_config['host'] = os.environ.get('DB-HOST')
+# db_config['host'] = os.environ.get('DB-HOST')
+db_config['unix_socket'] = os.environ.get('DB-CONNECTOR')
 db_config['database'] = os.environ.get('DB-NAME')
 
 db_dermis = mysql.connector.connect(**db_config)
